cb16ef9d082c18edc8e65856e1c3908279c210ee
acrefoot <acrefoot@zulip.com>
2014-01-22 16:06:31 -0500

    Fixup issues in response to Code Review

--- lib/services/humbug.rb
+++ lib/services/humbug.rb
@@ -2,7 +2,7 @@ class Service::Humbug < Service
   self.title = "Zulip"
   url = 'https://zulip.com'
   logo_url = 'https://zulip.com/static/images/logo/zulip-icon-512x512.png'
-  maintained_by :github => 'acrefoot'
+  maintained_by :github => 'zulip'
   supported_by :email => 'support@zulip.com'
 
   # textboxes
@@ -15,17 +15,17 @@ class Service::Humbug < Service
   string :alternative_endpoint
 
   # checkboxes
-  boolean :block_pull_requests
-  boolean :block_issues
-  boolean :block_commits
+  boolean :exclude_pull_requests
+  boolean :exclude_issues
+  boolean :exclude_commits
 
   # list of things approved for github's logging. See service.rb for more.
   white_list :email
   white_list :branches
   white_list :alternative_endpoint
-  white_list :block_pull_requests
-  white_list :block_issues
-  white_list :block_commits
+  white_list :exclude_pull_requests
+  white_list :exclude_issues
+  white_list :exclude_commits
 
   # events handled by this github hook
   default_events :commit_comment, :create, :delete, :download, :follow, :fork,
@@ -34,7 +34,6 @@ class Service::Humbug < Service
     :status
 
   def receive_event
-    puts data
     raise_config_error "Missing 'email'" if data['email'].to_s.empty?
     raise_config_error "Missing 'api_key'" if data['api_key'].to_s.empty?
 
@@ -43,10 +42,9 @@ class Service::Humbug < Service
     if data['alternative_endpoint'].empty?
         url = 'https://zulip.com/api/v1/external/github'
     else
-        url = data['alternative_endpoint'].to_s.strip
+        url = data['alternative_endpoint']
     end
-    puts url
-    puts data
+
     begin
         http.headers['User-Agent'] = 'ZulipGitHubWebhook'
         http.headers['Accept'] = 'application/json'
@@ -57,14 +55,14 @@ class Service::Humbug < Service
           :commit_stream => data['commit_stream'],
           :issue_stream => data['issue_stream'],
           :branches => data['branches'],
-          :block_pull_requests => data['block_pull_requests'],
-          :block_issues => data['block_issues'],
-          :block_commits => data['block_commits'],
+          :exclude_pull_requests => data['exclude_pull_requests'],
+          :exclude_issues => data['exclude_issues'],
+          :exclude_commits => data['exclude_commits'],
 
           :event => event,
           :payload => generate_json(payload),
-          # unspecified means an implicit version 1
-          :github_payload_version => '2',
+          # The Github payload version. Unspecified means an implicit version 1
+          :version => '2',
           :client => 'ZulipGitHubWebhook'
 
 
